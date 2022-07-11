31efb90fcab44011426605bcdabce653a29ead84
Pavel Fokin <fokinpavel2007@outlook.com>
2022-07-11 22:14:48 +0300

    5 commit

--- src/js/Logic.js
+++ src/js/Logic.js
@@ -1,26 +1,46 @@
+
 export default class Logic {
   constructor(gui) {
     this.gui = gui;
+    this.tickets = null;
     this.url = 'https://ahj-http-server-pfokin.herokuapp.com';
   }
 
   init() {
-    this.gui.btnAdd.addEventListener('click', (e) => {
+    this.getTickets();
+
+    this.gui.widget.addEventListener('click', (e) => {
       e.preventDefault();
-      if (this.gui.popUp.classList.contains('hidden')) {
-        this.gui.popUp.classList.remove('hidden');
+      console.log(e.target.dataset);
+      if(e.target.dataset.id === 'edit') {
+        this.editTicket(e);
+      } else if (e.target.dataset.id === 'remove') {
+        this.removeTicket(e);
+      } else if (e.target.dataset.id === 'title'){
+        this.showDescription(e);
+      } else if (e.target.dataset.id === 'addTicket') {
+        this.addTicket(e);
       }
+
+
     });
-    this.gui.btnCancel.addEventListener('click', (e) => {
-      e.preventDefault();
-      this.gui.popUp.classList.add('hidden');
-    });
-    this.gui.btnOk.addEventListener('click', (e) => {
-      e.preventDefault();
-      this.gui.popUp.classList.add('hidden');
-      const taskN = this.gui.createTask(1, true, 'ghjlydju jfdsoo', new Date().toString().slice(4, 21));
-      this.gui.tasksList.innerHTML += taskN;
-    });
+
+    // this.gui.btnAdd.addEventListener('click', (e) => {
+    //   e.preventDefault();
+    //   if (this.gui.popUp.classList.contains('hidden')) {
+    //     this.gui.popUp.classList.remove('hidden');
+    //   }
+    // });
+    // this.gui.btnCancel.addEventListener('click', (e) => {
+    //   e.preventDefault();
+    //   this.gui.popUp.classList.add('hidden');
+    // });
+    // this.gui.btnOk.addEventListener('click', (e) => {
+    //   e.preventDefault();
+    //   this.gui.popUp.classList.add('hidden');
+    //   const taskN = this.gui.createTask(1, true, 'ghjlydju jfdsoo', new Date().toString().slice(4, 21));
+    //   this.gui.tasksList.innerHTML += taskN;
+    // });
   }
 
   async sendXHR(method, query, type) {
@@ -41,7 +61,38 @@ export default class Logic {
     return (xhr.responseText);
   }
 
-  async modalSumbit(e) {
-    
+  async getTickets() {
+    const result = JSON.parse(await this.sendXHR('GET', 'allTickets'));
+    this.tickets = result;
+    this.fillFields((this.tickets));
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  editTicket(e) {
+    console.log('edit', e);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  removeTicket(e) {
+    console.log('remove', e);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  showDescription(e) {
+    console.log('showDescription', e);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  addTicket(e) {
+    console.log('addTicket', e);
+  }
+
+  fillFields(tArr) {
+    this.gui.tasksList.innerHTML = '';
+    tArr.forEach((ticket) => {
+      this.gui.tasksList.innerHTML += this.gui.createTask(
+        ticket.id, ticket.status, ticket.name, ticket.created,
+      );
+    });
   }
 }
